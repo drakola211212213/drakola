e51e16288546acd475ff59d4f0c92785e213e64a
mohamed <blackfighter394@gmail.com>
2019-12-24 03:27:29 +0200

    Updates

---
+++ precache-manifest.f3949c64777406c68602051f6a47c2f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195ebf3f5ad604e4d0a461262a8d7326",
+    "url": "/drakola211212213/drakola/index.html"
+  },
+  {
+    "revision": "5e50972e89ffb417b309",
+    "url": "/drakola211212213/drakola/static/css/main.eb1f196f.chunk.css"
+  },
+  {
+    "revision": "4f5b5c0ef152bf7df9cc",
+    "url": "/drakola211212213/drakola/static/js/2.0894c7bb.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/drakola211212213/drakola/static/js/2.0894c7bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e50972e89ffb417b309",
+    "url": "/drakola211212213/drakola/static/js/main.3e0c7181.chunk.js"
+  },
+  {
+    "revision": "0351c63883fffe0958d9",
+    "url": "/drakola211212213/drakola/static/js/runtime-main.0be8d54d.js"
+  }
+]);
